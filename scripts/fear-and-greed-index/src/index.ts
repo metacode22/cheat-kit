2131820e51496c9beb586a5f91ca74debc03550e
metacode22 <alohajune22@gmail.com>
2024-10-15 12:52:12 +0900

    fix: user agent 설정

--- scripts/fear-and-greed-index/src/index.ts
+++ scripts/fear-and-greed-index/src/index.ts
@@ -9,6 +9,9 @@ async function main() {
   });
 
   const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+  );
   await page.goto("https://www.cnn.com/markets/fear-and-greed");
 
   const fearAndGreedIndexTag = await page.waitForSelector(
